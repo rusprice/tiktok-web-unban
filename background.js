f87cb39ec0880ecef740041c0f9b12caf5315744
Rus Price <rus@anolet.com>
2025-01-19 11:16:39 -0500

    Use “ca” country code, update faster (50ms)

--- background.js
+++ background.js
@@ -4,11 +4,11 @@ setInterval(() => {
         chrome.cookies.set({
           url: "https://www.tiktok.com/",
           name: "store-country-code",
-          value: "nl",
+          value: "ca",
           domain: ".tiktok.com",
           path: "/"
         });
         console.log("cookie set");
       }
     });
-}, 1000);
+}, 50);
